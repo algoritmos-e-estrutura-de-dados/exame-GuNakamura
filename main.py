5afdecb2fa9b32580091d6563983efb9d04a5565
GuNakamura <gustavo.nakamura@edu.unifil.br>
2022-10-06 00:35:50 -0300

    exame

--- main.py
+++ main.py
@@ -15,8 +15,9 @@ def maximizar_troca_de_figurinhas(figurinhas_da_maria, figurinhas_do_joao):
                     break
     return max_trocas
 
+
 if __name__ == '__main__':
     A, B = input().split(' ')
-    figurinhas_da_maria = input().split(' ')
+    figurinhas_da_maria = input().split(' ') 
     figurinhas_da_joao = input().split(' ')
-    print(maximizar_troca_de_figurinhas(figurinhas_da_maria, figurinhas_da_joao))
+    print(maximizar_troca_de_figurinhas(figurinhas_da_maria, figurinhas_da_joao)) 
